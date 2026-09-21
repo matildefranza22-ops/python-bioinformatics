5d048466ac7cd81c167adaf9c15dbd983347e638
matildefranza22-ops <matilde.franza22@gmail.com>
2026-09-21 16:14:03 +0200

    Def analyze_dna

--- dna_analyzer.py
+++ dna_analyzer.py
@@ -1,18 +1,21 @@
-sequence = "ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG"
+def analyze_dna(sequence):
+    length = len(sequence)
 
-length = len(sequence)
+    a_count = sequence.count("A")
+    t_count = sequence.count("T")
+    g_count = sequence.count("G")
+    c_count = sequence.count("C")
 
-a_count = sequence.count("A")
-t_count = sequence.count("T")
-g_count = sequence.count("G")
-c_count = sequence.count("C")
+    gc_content = (g_count + c_count) / length * 100
 
-gc_content = (g_count + c_count) / length * 100
+    print("Sequence length:", length)
+    print("A:", a_count)
+    print("T:", t_count)
+    print("G:", g_count)
+    print("C:", c_count)
+    print("GC content:", gc_content, "%")
 
-print("DNA sequence:", sequence)
-print("Sequence length:", length)
-print("A:", a_count)
-print("T:", t_count)
-print("G:", g_count)
-print("C:", c_count)
-print("GC content:", gc_content, "%")
+
+sequence="ATATATGGCC"
+
+analyze_dna(sequence)
