d720f3e2b4c517e8ae4f201cd3293f82052f35a2
matildefranza22-ops <matilde.franza22@gmail.com>
2026-09-21 16:11:17 +0200

    Update DNA analysis

--- dna_analyzer.py
+++ dna_analyzer.py
@@ -2,5 +2,17 @@ sequence = "ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG"
 
 length = len(sequence)
 
+a_count = sequence.count("A")
+t_count = sequence.count("T")
+g_count = sequence.count("G")
+c_count = sequence.count("C")
+
+gc_content = (g_count + c_count) / length * 100
+
 print("DNA sequence:", sequence)
 print("Sequence length:", length)
+print("A:", a_count)
+print("T:", t_count)
+print("G:", g_count)
+print("C:", c_count)
+print("GC content:", gc_content, "%")
